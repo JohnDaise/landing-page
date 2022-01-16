f2c1f19773c192c16f6c5782bfc89111896915f4
JohnDaiseTruckerTools <jdaise@truckertools.com>
2022-01-16 17:30:05 -0500

    basic setup

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import Navbar from './Components/Navbar';
+
+function App() {
+
+  
+  return (
+    <div className="App">
+      <Navbar />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+      {/* <img src={logo} /> */}
+      {/* <Image src={require(`./spot.jpeg`)} alt={"spot"}/> */}
+     
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Navbar.js
@@ -0,0 +1,11 @@
+function Navbar() {
+
+    return (
+      <div className="App-header">
+         <img className="spot-img" src={require("../Assets/spot.jpeg")} alt={"spot"} />
+      </div>
+    );
+  }
+  
+  export default Navbar;
+  
