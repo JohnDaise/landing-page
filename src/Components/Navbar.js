49c07b0dcc03e13202b9393f2e1bbcc9eb29dcff
JohnDaiseTruckerTools <jdaise@gmail.com>
2022-01-16 17:53:27 -0500

    rename image file

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -2,7 +2,7 @@ function Navbar() {
 
     return (
       <div className="App-header">
-         <img className="spot-img" src={require("../Assets/Images/spot.jpeg")} alt={"spot"} />
+         <img className="spot-img" src={require("../Assets/Images/spotlight.jpeg")} alt={"spot"} />
       </div>
     );
   }
